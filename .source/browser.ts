6f3290ff281d148e1a84aeae3258cf4ebc04bf8d
phalla.doll <phalla.doll@gmail.com>
2026-04-04 11:00:15 +0700

    docs: remove per-component MDX docs in favor of example-based docs

--- .source/browser.ts
+++ .source/browser.ts
@@ -7,6 +7,6 @@ const create = browser<typeof Config, import("fumadocs-mdx/runtime/types").Inter
   }
 }>();
 const browserCollections = {
-  docs: create.doc("docs", {"index.mdx": () => import("../content/docs/index.mdx?collection=docs"), "installation.mdx": () => import("../content/docs/installation.mdx?collection=docs"), "theming.mdx": () => import("../content/docs/theming.mdx?collection=docs"), "components/alert-dialog.mdx": () => import("../content/docs/components/alert-dialog.mdx?collection=docs"), "components/badge.mdx": () => import("../content/docs/components/badge.mdx?collection=docs"), "components/button.mdx": () => import("../content/docs/components/button.mdx?collection=docs"), "components/card.mdx": () => import("../content/docs/components/card.mdx?collection=docs"), "components/dialog.mdx": () => import("../content/docs/components/dialog.mdx?collection=docs"), "components/input.mdx": () => import("../content/docs/components/input.mdx?collection=docs"), }),
+  docs: create.doc("docs", {"examples.mdx": () => import("../content/docs/examples.mdx?collection=docs"), "index.mdx": () => import("../content/docs/index.mdx?collection=docs"), "installation.mdx": () => import("../content/docs/installation.mdx?collection=docs"), "theming.mdx": () => import("../content/docs/theming.mdx?collection=docs"), }),
 };
 export default browserCollections;
--- .source/server.ts
+++ .source/server.ts
@@ -1,13 +1,8 @@
 // @ts-nocheck
-import * as __fd_glob_10 from "../content/docs/components/input.mdx?collection=docs"
-import * as __fd_glob_9 from "../content/docs/components/dialog.mdx?collection=docs"
-import * as __fd_glob_8 from "../content/docs/components/card.mdx?collection=docs"
-import * as __fd_glob_7 from "../content/docs/components/button.mdx?collection=docs"
-import * as __fd_glob_6 from "../content/docs/components/badge.mdx?collection=docs"
-import * as __fd_glob_5 from "../content/docs/components/alert-dialog.mdx?collection=docs"
-import * as __fd_glob_4 from "../content/docs/theming.mdx?collection=docs"
-import * as __fd_glob_3 from "../content/docs/installation.mdx?collection=docs"
-import * as __fd_glob_2 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/theming.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/installation.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_2 from "../content/docs/examples.mdx?collection=docs"
 import { default as __fd_glob_1 } from "../content/docs/components/meta.json?collection=docs"
 import { default as __fd_glob_0 } from "../content/docs/meta.json?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
@@ -18,4 +13,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "components/meta.json": __fd_glob_1, }, {"index.mdx": __fd_glob_2, "installation.mdx": __fd_glob_3, "theming.mdx": __fd_glob_4, "components/alert-dialog.mdx": __fd_glob_5, "components/badge.mdx": __fd_glob_6, "components/button.mdx": __fd_glob_7, "components/card.mdx": __fd_glob_8, "components/dialog.mdx": __fd_glob_9, "components/input.mdx": __fd_glob_10, });
+export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, "components/meta.json": __fd_glob_1, }, {"examples.mdx": __fd_glob_2, "index.mdx": __fd_glob_3, "installation.mdx": __fd_glob_4, "theming.mdx": __fd_glob_5, });
